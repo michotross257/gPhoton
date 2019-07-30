c1f664240e2bbd580b4a80b35ba2cd2300d67940
Michael Trossbach <michotross257@gmail.com>
2019-07-30 16:25:09 -0400

    Initial commit

--- upload_parquet_files_to_s3.py
+++ upload_parquet_files_to_s3.py
@@ -6,7 +6,7 @@ import argparse
 parser = argparse.ArgumentParser(description='Get AWS info and directory of PARQUET files.')
 parser.add_argument('region', help='Name of AWS region.')
 parser.add_argument('-p', '--profile', metavar='', default='default',
-                    help='Name of AWS profile.')
+                    help='Name of AWS profile (default: "default").')
 parser.add_argument('bucket', help='Name of AWS S3 bucket to which the PARQUET files will be uploaded.')
 parser.add_argument('folder', help='Path to folder where PARQUET files are stored.')
 args = parser.parse_args()
@@ -18,9 +18,9 @@ client = sess.client('s3')
 
 for file in os.listdir(args.folder):
 	command = [
-        'aws', 's3', 'cp',
-        os.path.join(args.folder, file),
-        's3://{}'.format(os.path.join(args.bucket, file)),
-        '--profile', args.profile
-    ]
+		'aws', 's3', 'cp',
+		os.path.join(args.folder, file),
+		's3://{}'.format(os.path.join(args.bucket, file)),
+		'--profile', args.profile
+	]
 	subprocess.run(command)

c9597531c745217c1b3dc945a783b66012703803
Michael Trossbach <michotross257@gmail.com>
2019-07-31 12:13:29 -0400

    Incorporated multiprocessing functionality

--- upload_parquet_files_to_s3.py
+++ upload_parquet_files_to_s3.py
@@ -1,7 +1,9 @@
 import os
 import boto3
+import multiprocessing as mp
 import subprocess
 import argparse
+import numpy as np
 
 parser = argparse.ArgumentParser(description='Get AWS info and directory of PARQUET files.')
 parser.add_argument('region', help='Name of AWS region.')
@@ -9,18 +11,47 @@ parser.add_argument('-p', '--profile', metavar='', default='default',
                     help='Name of AWS profile (default: "default").')
 parser.add_argument('bucket', help='Name of AWS S3 bucket to which the PARQUET files will be uploaded.')
 parser.add_argument('folder', help='Path to folder where PARQUET files are stored.')
+parser.add_argument('-m', '--multiprocessing', action='store_true',
+                    help='Whether to use multiprocessing to do the upload.')
 args = parser.parse_args()
 
+def file_upload(file_names):
+    '''
+        Upload files to S3.
+        
+        Parameters
+        ----------
+        file_names: list
+            name(s) of file(s) to be uploaded
+        
+        Returns
+        -------
+        None
+    '''
+    for file in file_names:
+        command = [
+            'aws', 's3', 'cp',
+            os.path.join(args.folder, file),
+            's3://{}'.format(os.path.join(args.bucket, file)),
+            '--profile', args.profile
+        ]
+        subprocess.run(command)
 
-sess = boto3.Session(region_name=args.region,
-                     profile_name=args.profile)
-client = sess.client('s3')
+if __name__ == '__main__':
+    sess = boto3.Session(region_name=args.region,
+                         profile_name=args.profile)
+    client = sess.client('s3')
 
-for file in os.listdir(args.folder):
-	command = [
-		'aws', 's3', 'cp',
-		os.path.join(args.folder, file),
-		's3://{}'.format(os.path.join(args.bucket, file)),
-		'--profile', args.profile
-	]
-	subprocess.run(command)
+    files = sorted(os.listdir(args.folder))
+    files = list(filter(lambda x: x.endswith('parquet'), files))
+    if args.multiprocessing:
+        processing_segments = np.linspace(0, len(files), mp.cpu_count()+1).astype(int)
+        processes = []
+        for index in range(mp.cpu_count()):
+            start, stop = processing_segments[index], processing_segments[index+1]
+            processes.append(mp.Process(target=file_upload,
+                                        args=(files[start: stop])))
+        [p.start() for p in processes]
+        [p.join() for p in processes]
+    else:
+        file_upload(files)

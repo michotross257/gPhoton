5e246dca30d7ca7f8bf777549c70f64e4f0be536
Michael Trossbach <michotross257@gmail.com>
2019-07-31 12:12:42 -0400

    Modified to add multiprocessing functionality

--- acquire_gPhoton_csv_files.py
+++ acquire_gPhoton_csv_files.py
@@ -3,6 +3,7 @@ import sys
 import requests
 import csv
 import argparse
+import multiprocessing as mp
 import numpy as np
 import pandas as pd
 import pyarrow as pa
@@ -14,11 +15,22 @@ parser.add_argument('save_path', help='Path to folder where to save PARQUET file
 args = parser.parse_args()
 header = ['zoneID', 'time', 'cx', 'cy', 'cz', 'x', 'y', 'xa', 'ya', 'q', 'xi', 'eta', 'ra', 'dec', 'flag']
 
-if __name__ == '__main__':
-    with open(args.txt_file) as txt_file:
-        file_names = list(filter(lambda x: len(x), txt_file.read().split('\n')))
+def mp_content_download(file_names):
+    '''
+        Uses multiprocessing module to download CSVs and incrementally create parquet
+        files from the files.
+        
+        Parameters
+        ----------
+        file_names: list
+            name(s) of file(s) to be downloaded by the multiprocessing process
+        
+        Returns
+        -------
+        None
+    '''
     for file_name in file_names:
-        print('\nDownloading content from:', file_name)
+        print('\nDownloading content from: {}'.format(file_name))
         # ----- lazy -----
         downloaded_csv = requests.get(file_name, stream=True)
         lines = (line.decode('utf-8') for line in downloaded_csv.iter_lines())
@@ -39,10 +51,21 @@ if __name__ == '__main__':
                 collected_data = []
                 inc += 1
             else:
-                msg = '\rReading row #{:,}'.format(cnt)
-                buffer = msg_len - len(msg)
-                buffer = buffer if buffer > 0 else 0
-                print(msg, end=' '*buffer, flush=True)
-                msg_len = len(msg)
+                #msg = '\rReading row #{:,}'.format(cnt)
+                #buffer = msg_len - len(msg)
+                #buffer = buffer if buffer > 0 else 0
+                #print(msg, end=' '*buffer, flush=True)
+                #msg_len = len(msg)
                 collected_data.append(row)
-        print('='*100)
+
+if __name__ == '__main__':
+    with open(args.txt_file) as txt_file:
+        file_names = list(filter(lambda x: len(x), txt_file.read().split('\n')))
+    processing_segments = np.linspace(0, len(file_names), mp.cpu_count()+1).astype(int)
+    processes = []
+    for index in range(mp.cpu_count()):
+        start, stop = processing_segments[index], processing_segments[index+1]
+        processes.append(mp.Process(target=mp_content_download,
+                                    args=(file_names[start: stop],)))
+    [p.start() for p in processes]
+    [p.join() for p in processes]

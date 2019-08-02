273c31b8d813bdfbb5dafe4d60f9585612972486
Michael Trossbach <michotross257@gmail.com>
2019-08-02 11:59:54 -0400

    Initial commit

---
+++ upload_files_to_s3.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+FILES=$1
+BUCKET=$2
+PROFILE=${3:-default}
+for f in $FILES/*
+do
+    FILENAME=`basename ${f%%%}`
+    S3_PATH="${BUCKET}/${FILENAME}"
+    echo "Uploading ${f} to ${S3_PATH}"
+    aws s3 cp $f $S3_PATH --profile $PROFILE
+done

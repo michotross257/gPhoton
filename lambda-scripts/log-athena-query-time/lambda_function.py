4e3f61518e54344a886636442bd85dbb849d00bb
Michael Trossbach <michotross257@gmail.com>
2019-08-05 10:23:06 -0400

    Initial commit

---
+++ lambda-scripts/log-athena-query-time/lambda_function.py
@@ -0,0 +1,28 @@
+import os
+import boto3
+from datetime import datetime, timezone
+
+athena_table_id = os.environ.get('ATHENA_TABLE')
+profile_name = os.environ.get('PROFILE_NAME')
+region_name = os.environ.get('REGION_NAME')
+client = boto3.client('dynamodb')
+
+def lambda_handler(event, context):
+    end_time = datetime.now(timezone.utc)
+    end_time = end_time.strftime('%H:%M:%S.%f %p %Z')
+    key = event['Records'][0]['s3']['object']['key']
+    execution_id = key.split('/')[-1].replace('.csv', '')
+    client.update_item(
+        TableName=athena_table_id,
+        Key={
+            'EXECUTION_ID':{
+                    'S': execution_id
+            }
+        },
+        UpdateExpression='SET EXECUTION_END_TIME = :et',
+        ExpressionAttributeValues={
+            ":et": {
+                'S': str(end_time)
+            }
+        }
+    )

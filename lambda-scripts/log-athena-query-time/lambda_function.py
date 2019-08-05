d033da2106e5e52d468ca9d8b9ab06309dedbbd8
Michael Trossbach <michotross257@gmail.com>
2019-08-05 15:16:31 -0400

    Moved AWS credential access from outside of the lambda function to inside

--- lambda-scripts/log-athena-query-time/lambda_function.py
+++ lambda-scripts/log-athena-query-time/lambda_function.py
@@ -2,13 +2,15 @@ import os
 import boto3
 from datetime import datetime, timezone
 
-athena_table_id = os.environ.get('ATHENA_TABLE')
-profile_name = os.environ.get('PROFILE_NAME')
-region_name = os.environ.get('REGION_NAME')
-client = boto3.client('dynamodb')
-
 def lambda_handler(event, context):
+    # before anything else, let's get the current time
     end_time = datetime.now(timezone.utc).isoformat(timespec='microseconds')
+    
+    athena_table_id = os.environ.get('ATHENA_TABLE')
+    profile_name = os.environ.get('PROFILE_NAME')
+    region_name = os.environ.get('REGION_NAME')
+    client = boto3.client('dynamodb')
+    
     key = event['Records'][0]['s3']['object']['key']
     execution_id = key.split('/')[-1].replace('.csv', '')
     client.update_item(

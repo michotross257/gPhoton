08548f158ea95e1eddd87cdecb40c046b3e92ae7
Michael Trossbach <michotross257@gmail.com>
2019-08-07 13:23:04 -0400

    Included additional attributes to write to DynamoDB table

--- lambda-scripts/log-athena-query-time/lambda_function.py
+++ lambda-scripts/log-athena-query-time/lambda_function.py
@@ -6,24 +6,38 @@ def lambda_handler(event, context):
     # before anything else, let's get the current time
     end_time = datetime.now(timezone.utc).isoformat(timespec='microseconds')
     
+    ATHENA_COST_PER_BYTE = 5/1e+12 # $5/TB scanned
     athena_table_id = os.environ.get('ATHENA_TABLE')
     profile_name = os.environ.get('PROFILE_NAME')
     region_name = os.environ.get('REGION_NAME')
-    client = boto3.client('dynamodb')
+    athena_client = boto3.client('athena')
+    db_client = boto3.client('dynamodb')
     
     key = event['Records'][0]['s3']['object']['key']
     execution_id = key.split('/')[-1].replace('.csv', '')
-    client.update_item(
+    
+    response = athena_client.get_query_execution(
+        QueryExecutionId=execution_id
+    )
+    bytes_scanned = int(response['QueryExecution']['Statistics']['DataScannedInBytes'])
+    
+    db_client.update_item(
         TableName=athena_table_id,
         Key={
             'EXECUTION_ID':{
                     'S': execution_id
             }
         },
-        UpdateExpression='SET EXECUTION_END_TIME = :et',
+        UpdateExpression='SET EXECUTION_END_TIME=:et, QUERY_COST=:cost, DATA_SCANNED=:data',
         ExpressionAttributeValues={
             ':et': {
                 'S': end_time
+            },
+            ':cost': {
+                'S': '${:.10f}'.format(bytes_scanned*ATHENA_COST_PER_BYTE)
+            },
+            ':data': {
+                'S': '{:.10f} GB'.format(bytes_scanned*1e-9)
             }
         }
     )

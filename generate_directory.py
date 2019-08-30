41bde60e4b0339947535174232b10c8d9f70750d
Michael Trossbach <michotross257@gmail.com>
2019-08-30 15:07:24 -0400

    Commented out step to generate dec partitions

--- generate_directory.py
+++ generate_directory.py
@@ -1,23 +1,10 @@
 import os
 import sys
 
-PATH = sys.argv[1]
-partition_factor = 10
-zoneIDs = [10829, 10830, 10831]
-ra = {
-    'id': 'ra',
-    'min': 0,
-    'max': 360
-}
-ra['step'] = int((ra['max'] - ra['min'])/partition_factor)
-dec = {
-    'id': 'dec',
-    'min': -90,
-    'max': 90
-}
-dec['step'] = int((dec['max'] - dec['min'])/partition_factor)
-
 def make_folders(partitions, index=0, paths=None):
+    '''
+    Recursively create the directory structure and then make the directories.
+    '''
     if paths is None:
         paths = [PATH]
     temp_paths = []
@@ -36,6 +23,9 @@ def make_folders(partitions, index=0, paths=None):
     make_folders(partitions=partitions, index=index+1, paths=temp_paths)
 
 def get_partitions(params):
+    '''
+    For a given ID, create a list of partitions based on the min, max, and step values.
+    '''
     partitions = []
     for val in range(params['min'], params['max'], params['step']):
         if val == (params['max'] - params['step']):
@@ -44,10 +34,26 @@ def get_partitions(params):
             partitions.append('{}<={}<{}'.format(val, params['id'], val+params['step']))
     return partitions
 
-p = [
-        ['zoneID={}'.format(id) for id in zoneIDs],
-        get_partitions(ra),
-        get_partitions(dec)
-    ]
+if __name__ == '__main__':
+    PATH = sys.argv[1]
+    partition_factor = 10
+    zoneIDs = [10829, 10830, 10831]
+    ra = {
+        'id': 'ra',
+        'min': 0,
+        'max': 360
+    }
+    ra['step'] = int((ra['max'] - ra['min'])/partition_factor)
+    dec = {
+        'id': 'dec',
+        'min': -90,
+        'max': 90
+    }
+    dec['step'] = int((dec['max'] - dec['min'])/partition_factor)
 
-make_folders(p)
+    p = [
+         ['zoneID={}'.format(id) for id in zoneIDs],
+         get_partitions(ra),
+         #get_partitions(dec)
+    ]
+    make_folders(p)

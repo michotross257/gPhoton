43969581dc5f737244dcc7fe748ec2d367edd84c
Michael Trossbach <michotross257@gmail.com>
2019-08-30 15:44:36 -0400

    Preappended 's3://' to S3 path

--- upload_files_to_s3.sh
+++ upload_files_to_s3.sh
@@ -5,7 +5,7 @@ PROFILE=${3:-default}
 for f in $FILES/*
 do
     FILENAME=`basename ${f%%%}`
-    S3_PATH="${BUCKET}/${FILENAME}"
+    S3_PATH="s3://${BUCKET}/${FILENAME}"
     echo "Uploading ${f} to ${S3_PATH}"
     aws s3 cp $f $S3_PATH --profile $PROFILE
 done
